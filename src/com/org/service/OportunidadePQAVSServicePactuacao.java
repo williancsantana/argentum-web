11fa7a8ec510d8ffa11870e48351b467d75d0481
João Henrique Morais <jhmorais.09@gmail.com>
2017-12-18 16:51:35 -0200

    teste de comentario

--- src/com/org/service/OportunidadePQAVSServicePactuacao.java
+++ src/com/org/service/OportunidadePQAVSServicePactuacao.java
@@ -1,9 +1,9 @@
-/*
+﻿/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package com.org.service;
-
+//teste
 import com.linuxense.javadbf.DBFException;
 import com.linuxense.javadbf.DBFField;
 import com.linuxense.javadbf.DBFReader;
