2d1821d17d0bed1951a5ce579c3b7674ab7bc794
cidsantos <cid.paulo@gmail.com>
2017-12-19 14:59:23 -0200

    correcao relatorio hanseniase contatos examinados

--- src/com/org/model/classes/agravos/ContatosExaminadosHanseniasePactuacao.java
+++ src/com/org/model/classes/agravos/ContatosExaminadosHanseniasePactuacao.java
@@ -524,20 +524,20 @@ public class ContatosExaminadosHanseniasePactuacao extends Agravo {
 
     @Override
     public String[] getOrdemColunas() {
-        return new String[]{"COUUFINF", "ID_LOCRES", "DS_LOCRES", "COD_CIR", "NOME_CIR", "D_HANSREG", "N_HANSEXAM", "P_HANSEXAM", "ANO_DIAG", "DT_DIAGIN", "DT_DIAGFI", "ORIGEM"};
+        return new String[]{"ID_UFRES", "ID_LOCRES", "DS_LOCRES", "COD_CIR", "NOME_CIR", "D_HANREG", "N_HANSEXAM", "P_CONEXA", "ANO_DIAG", "DT_DIAGIN", "DT_DIAGFI", "ORIGEM"};
     }
 
     @Override
     public HashMap<String, ColunasDbf> getColunas() {
         HashMap<String, ColunasDbf> hashColunas = new HashMap<String, ColunasDbf>();
-        hashColunas.put("COUUFINF", new ColunasDbf(30));
+        hashColunas.put("ID_UFRES", new ColunasDbf(30));
         hashColunas.put("ID_LOCRES", new ColunasDbf(30));
         hashColunas.put("DS_LOCRES", new ColunasDbf(30));
         hashColunas.put("COD_CIR", new ColunasDbf(30));
         hashColunas.put("NOME_CIR", new ColunasDbf(30));
-        hashColunas.put("D_HANSREG", new ColunasDbf(30));
+        hashColunas.put("D_HANREG", new ColunasDbf(30));
         hashColunas.put("N_HANSEXAM", new ColunasDbf(30));
-        hashColunas.put("P_HANSEXAM", new ColunasDbf(30));
+        hashColunas.put("P_CONEXA", new ColunasDbf(30));
         hashColunas.put("ANO_DIAG", new ColunasDbf(30));
         hashColunas.put("DT_DIAGIN", new ColunasDbf(30));
         hashColunas.put("DT_DIAGFI", new ColunasDbf(30));
